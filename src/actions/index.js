93e62a09a36e03abbace54b34020ca1259ed423b
cuteyuruko <cuteyuruko@gmail.com>
2022-01-18 00:01:43 +0900

    implement action and action creator

---
+++ src/actions/index.js
@@ -0,0 +1,13 @@
+//以下Action
+//Actionを返す関数をActionCreate
+export const INCREMENT = 'INCREMENT'
+export const DECREMENT = 'DECREMENT'
+const increment = () =>({
+        type: INCREMENT
+    })
+
+const decrement = () => (
+    {
+        type: DECREMENT
+    }
+)
